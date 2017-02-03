8cceac3da0aa1f45c133f90a7b091ddacad86728
Kai Rautenberg <mail@kairautenberg.de>
2017-02-03 17:41:25 +0100

    +Vuex store

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexFire from 'vuexfire'
+import Firebase from 'firebase'
+import VueRouter from 'vue-router'
+
 import App from './App'
 import Goals from './components/Goals'
 import Achievements from './components/Achievements'
-
-import VueFire from 'vuefire'
-import Firebase from 'firebase'
-import VueRouter from 'vue-router'
 
 let firebaseApp = Firebase.initializeApp({
   apiKey: 'AIzaSyD1J5m2PaKJGahr_YIsphKkb2r9D33ZUUQ',
@@ -18,36 +19,54 @@ let firebaseApp = Firebase.initializeApp({
 })
 let db = firebaseApp.database()
 
-Vue.use(VueFire)
+Vue.use(Vuex)
+Vue.use(VuexFire)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      component: Goals
-    },
-    {
-      path: '/goals',
-      component: Goals
-    },
+    {path: '/', component: Goals},
+    {path: '/goals', component: Goals},
     {path: '/achievements', component: Achievements}
   ]
+})
+
+const store = new Vuex.Store({
+  state: {
+    goals: null,
+    goalsRef: null
+  },
+  mutations: {
+    ...VuexFire.mutations,
+    addGoal: function (state, {ref, payload}) {
+      ref.push(payload)
+    }
+  },
+  getters: {
+    goals: function (state) { return state.goals }
+  }
 })
 
 /* eslint-disable no-new */
 new Vue({
   router,
+  store,
   el: '#app',
   template: '<App />',
   components: { App },
   methods: {
     addGoal: function (name) {
-      this.$firebaseRefs.goals.push({name: name})
+      store.commit('addGoal', {ref: this.$firebaseRefs['goals'], payload: {name: name}})
     }
   },
+  computed: Vuex.mapGetters([
+    'goals'
+  ]),
   firebase: {
     goals: db.ref('goals')
+  },
+  beforeCreate: function () {
+    store.commit('setRef', db.ref('goals'))
   }
 })
